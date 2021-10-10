150af89ac3a33fe78abbcf68d53824528f8de06b
Samstix636 <41683444+Samstix636@users.noreply.github.com>
2021-10-10 15:24:20 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 INFURA_URL = 'Your INFURA_URL'
 
-private = 'Enter Your Wallet Private Key' # Should look like this 806e1a8a340827dfa3b671fa98d3fe24f45d585f2583426eae75d24ea667de5c
+private = 'Enter Your Wallet Private Key' # Should look like this 806e1a8a340827dfa3b671fa98**********585f2583426eae75d24ea667de5c
